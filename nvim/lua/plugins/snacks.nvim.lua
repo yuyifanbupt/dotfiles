79287251cb45fbd887999cc8152c4eab0ea0ca83
yuyifan <yuyifanbupt@gmail.com>
2026-07-29 13:54:54 +0800

    update snacks.nvim configuration

--- nvim/lua/plugins/snacks.nvim.lua
+++ nvim/lua/plugins/snacks.nvim.lua
@@ -4,6 +4,31 @@ return {
     picker = {
       hidden = true,
       ignored = true,
+      sources = {
+        files = {
+          hidden = true,
+          ignored = true,
+        },
+      },
+      win = {
+        -- input window
+        input = {
+          keys = {
+            -- to close the picker on ESC instead of going to normal mode,
+            -- add the following keymap to your config
+            -- ["<Esc>"] = { "close", mode = { "n", "i" } },
+            ["H"] = { "toggle_hidden", mode = { "n" } },
+            ["I"] = { "toggle_ignored", mode = { "n" } },
+          },
+        },
+        -- result list window
+        list = {
+          keys = {
+            ["H"] = "toggle_hidden",
+            ["I"] = "toggle_ignored",
+          },
+        },
+      },
     },
   },
 }
