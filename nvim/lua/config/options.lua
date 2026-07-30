b30ff25b8cd40456c638d8e531980c15457f610b
yuyifan <yuyifanbupt@gmail.com>
2026-07-30 12:43:35 +0800

    readonly config for nvim

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,3 +1,11 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.api.nvim_create_autocmd({ "BufRead" }, {
+  pattern = { "*/.venv/*", "*/node_modules/*", "*/dist/*" },
+  callback = function()
+    vim.bo.modifiable = false
+    vim.bo.readonly = true
+  end,
+})
