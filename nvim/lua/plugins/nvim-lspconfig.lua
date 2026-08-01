41402ad719c5196c392cbde074118d6e8a65ffe2
yuyifan <yuyifanbupt@gmail.com>
2026-08-01 22:07:34 +0800

    zt after lsp_definition jump

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -2,6 +2,28 @@ return {
   "neovim/nvim-lspconfig",
   opts = {
     servers = {
+      ["*"] = {
+        keys = {
+          {
+            "gd",
+            function()
+              Snacks.picker.lsp_definitions({
+                jump = {
+                  reuse_win = true,
+                  tagstack = true,
+                },
+                confirm = function(picker, item)
+                  picker:close()
+                  vim.cmd("edit " .. item.file)
+                  vim.api.nvim_win_set_cursor(0, { item.pos[1], item.pos[2] })
+                  vim.cmd("normal! zt")
+                end,
+              })
+            end,
+            desc = "Goto Definition",
+          },
+        },
+      },
       -- pylsp = {
       --   settings = {
       --     pylsp = {
