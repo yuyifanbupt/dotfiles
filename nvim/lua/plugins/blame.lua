470830e5f5f3bda3d6f402ceb0218f4dd0e10982
yuyifan <yuyifanbupt@gmail.com>
2026-08-01 22:40:11 +0800

    update blame.nvim date_format

--- nvim/lua/plugins/blame.lua
+++ nvim/lua/plugins/blame.lua
@@ -3,7 +3,9 @@ return {
     "FabijanZulj/blame.nvim",
     lazy = false,
     config = function()
-      require("blame").setup({})
+      require("blame").setup({
+        date_format = "%Y-%m-%d",
+      })
     end,
     keys = {
       { "<leader>gb", "<cmd>BlameToggle<cr>", mode = { "n" }, desc = "git blame toggle" },
