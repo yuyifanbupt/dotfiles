c131188737598b4da96f7c96352a77801a7820e8
yuyifan <yuyifanbupt@gmail.com>
2026-08-29 03:04:36 +0800

    fix <c-l> in nvim terminal

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -16,3 +16,9 @@ vim.api.nvim_create_autocmd({
     vim.cmd("checktime")
   end,
 })
+
+vim.api.nvim_create_autocmd("TermEnter", {
+  callback = function(ev)
+    vim.keymap.set("t", "<c-l>", "<c-l>", { buffer = ev.buf, nowait = true })
+  end,
+})
